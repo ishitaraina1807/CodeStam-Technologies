89957a6e3219f0ba08f2e75cf66fdcffe60b27b0
Eklavya-sus <eklavya0304@gmail.com>
2023-10-17 17:29:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Pillar from './components/pillar';
+
+function App() {
+  return (
+  <div>
+    <Pillar/>
+  </div>
+
+
+  );
+}
+
+export default App;
---
+++ src/components/pillar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from './card'
+
+export default function pillar() {
+  return (
+    <div className='flex flex-wrap'> 
+      <Card/>
+      {/* <Card/>
+      <Card/>
+      <Card/> */}
+    </div>
+  )
+}
